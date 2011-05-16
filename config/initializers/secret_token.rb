e5ef4fbf3ffc3b5ddf14545b5f84fb6f01dd8664
Andrew Gillibrand (ADRGLT) <andrew@upstream.info>
2011-05-16 19:07:01 +0200

    first test

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BloomingWater309::Application.config.secret_token = '705a12176097eba6b92f275641ca9748442642991e4b0623212347fab53c7e0512262310a7d77a8b079b0f9345b6c2180ffbfc0c4715217677ec80299b52009b'
